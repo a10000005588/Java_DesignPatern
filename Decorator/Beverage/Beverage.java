3c77dbb07653c263ddca8d75dd83946bb1e8a2b5
will <a10000005588@gmail.com>
2019-11-02 23:13:59 +0800

    [add] decorator pattern

---
+++ Decorator/Beverage/Beverage.java
@@ -0,0 +1,14 @@
+package Beverage;
+
+public abstract class Beverage {
+    String description = "Unknown Beverage";
+    double cost = 0;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double cost() {
+        return cost;
+    }
+}
---
+++ Decorator/CondimentDecorator/CondimentDecorator.java
@@ -0,0 +1,7 @@
+package CondimentDecorator;
+
+import Beverage.Beverage;
+
+public abstract class CondimentDecorator extends Beverage {
+    public abstract String getDescription();
+}
